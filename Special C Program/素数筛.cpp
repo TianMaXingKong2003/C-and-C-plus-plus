38344baf6d69e3531871eae1cc7cf59bc6379362
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2017-09-26 19:57:11 +0800

    Add files via upload

---
+++ Special C Program/素数筛.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#define max 100001
+
+int main()
+{
+	bool prime[max];prime[0]=0;prime[1]=0;prime[2]=1;prime[3]=1;
+	
+	int i,j;
+	
+	for(i=3;i<max;i++)
+		prime[i]=i%2==0?0:1;
+	
+	int t=(int) sqrt(max);
+	
+	for(i=3;i<=t;i++)
+	{
+		if(prime[i])
+			for(j=i*i;j<=max;j=j+2*i)
+			{
+				prime[j]=0;
+				//printf("%d不是一个素数\n",j);
+			}
+	}
+	
+	
+	for(i=1;i<1000;i++)
+	{
+		if(prime[i])
+			printf("素数为：%d    prime[%d]\n",i,i);
+		//else
+		//	printf("prime[%d]     不是素数\n",i);
+	}
+ } 
