7d4934988bf1c9246d73709b3b6e0b0503c679e9
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2017-11-07 09:44:16 +0800

    2017-10-27

--- 平时代码/2017蓝桥杯/放苹果问题.cpp
+++ 平时代码/2017蓝桥杯/放苹果问题.cpp
@@ -1,45 +1,54 @@
 #include<stdio.h>
 
-int fun(int n,int m)
+int f(int m,int n)
 {
-    if (n==0&&m!=0)
-       return 0;
-    else if (m==0)
-    {
-      return 1;   
-    }
-    else if (m>=n)
-    {
-      return fun(n,m-n)+fun(n-1,m);
-    }
-   else
-      return fun(m,m);  
- 
+	
+	if(m==0)
+	{
+		//	printf("f(%d,%d)\n",m,n);
+			return 1;
+	}
+	
+	else	if(n==0&&m!=0)	
+	{
+	//	printf("f(%d,%d)\n",m,n);
+		return 0;
+	}
+	
+	else 	if(m<n)		
+	{
+	//	printf("f(%d,%d)\n",m,m);
+		return f(m,m);
+	}
+	
+	else	if(m>=n)
+	
+	{
+	//	printf("f(%d,%d)+f(%d,%d)\n",m,n-1,m-n,n);
+		return f(m,n-1)+f(m-n,n);
+	}
 }
 
 int main()
 {
-	int c;
-	scanf("%d",&c);
-	 
-	 int m[1000],n[1000],k[1000];
-	 
-	 
-	int i=0;
-	for(;i<c;i++)
-	{	
-		scanf("%d%d",&m[i],&n[i]);k[i]=0;
-		
-		k[i]=fun(n[i],m[i]);
-	 } 
-	 
-	 for(i=0;i<c;i++)
-	 {
-	 	if(i==c-1)
-	 		printf("%d",k[i]);
-	 	else
-	 		printf("%d\n",k[i]);
-	 }
-}
-
-
+	int c;scanf("%d",&c);int i,j;int out[100];
+	
+	for(i=0;i<c;i++)
+	{
+		int m,n;scanf("%d%d",&m,&n);
+		out[i]=f(m,n);
+	}
+	
+	for(j=0;j<c;j++)
+	{
+			printf("%d",out[j]); 
+			if(j!=c-1)	printf("\n");
+	}
+	
+	
+ } 
+ 
+ 
+ 
+ 
+ 
