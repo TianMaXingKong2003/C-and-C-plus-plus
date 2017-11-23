1a0088d3bf65a2886500b79c4733f211e529fdc1
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2017-11-23 19:57:26 +0800

    2017-9-24

--- Special C Program/算闰年.cpp
+++ Special C Program/算闰年.cpp
@@ -3,12 +3,16 @@ int main()
 {
 	int i,j,x;
 	j=0;
-	for(i=1998;i<1998+76;i++)
+	for(i=1984;i<=1998+76;i++)
 	{
-		if(i%4==0&&i%400!=0)
+		if(i%4==0)
 		{
-			j++;
-			printf("%d    %d\n",i,j);	
+			if(i%100==0&&i%400!=0)	;
+			else
+			{
+				j++;
+				printf("%d    %d\n",i,j);	
+			}
 		}
 		
 		
