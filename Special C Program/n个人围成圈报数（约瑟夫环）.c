6347bdb5070b1c497ebb4f6e8c2af9c35ebaa009
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2018-11-23 21:38:06 +0800

    Create n个人围成圈报数（约瑟夫环）.c

---
+++ Special C Program/n个人围成圈报数（约瑟夫环）.c
@@ -0,0 +1,24 @@
+#include<iostream>
+
+//	样例测试	
+//	6	3	->	1
+//	7	3	->	4
+//	8	3	->	7
+//	9	3	->	1
+//	10	4	->	5
+
+using namespace std;
+
+int solve(int n,int m)
+{
+	int r=0;
+	for(int i=2;i<=n;i++)
+		r=(r+m)%i;
+	return r+1;
+}
+
+int main()
+{
+	cout<<solve(8,3);
+	return 0;
+}
